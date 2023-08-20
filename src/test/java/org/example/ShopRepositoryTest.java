6e788c528584e49e018ca914dd18796fe6c5434a
Maksis94 <palchenkov94@gmail.com>
2023-08-20 12:47:14 +0300

    выполнение задания

---
+++ src/test/java/org/example/ShopRepositoryTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShopRepositoryTest {
+    Product product1 = new Product(1, "Продукт 1", 1000);
+    Product product2 = new Product(2, "Продукт 2", 2000);
+    Product product3 = new Product(3, "Продукт 3", 3000);
+
+    ShopRepository repository = new ShopRepository();
+
+    @Test
+    public void shouldDeleteExistingElement() {
+        repository.add(product1);
+        repository.add(product2);
+        repository.add(product3);
+
+        repository.remove(2);
+        Assertions.assertArrayEquals(repository.findAll(), new Product[]{product1, product3});
+    }
+
+    @Test
+    public void shouldThrowNotFoundException() {
+        repository.add(product1);
+        repository.add(product2);
+        repository.add(product3);
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repository.remove(100);
+        });
+    }
+}
